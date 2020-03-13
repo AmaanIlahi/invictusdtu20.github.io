a0b423115dd1f6c4bf0d454c9386fc9c90938e56
AmaanIlahi <amaan1745@gmail.com>
2020-03-13 08:57:04 +0530

    first commit

---
+++ js/events.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+
+
+AOS.init({
+        offset: 400, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 2000 // values from 0 to 3000, with step 50ms
+      });
